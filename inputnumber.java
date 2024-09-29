e1cfd398b3a2e1d175876663589778d13032eb4e
Anand Mahato <167707816+Anandkumarm0001@users.noreply.github.com>
2024-09-29 09:46:07 +0530

    Add files via upload

---
+++ inputnumber.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class main {
+	 public static void main(String[] args){
+		 Scanner input = new Scanner(System.in);
+		 System.out.println("please enter your name");
+		 String Username = input .next();
+		System.out.println("Your name is : " + Username);
+	 }
+}
